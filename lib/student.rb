4953dfe66361959973026b57dd24e1dac9e8beae
remote-torrent-6747 <abbyc@sky.com>
2021-03-16 22:12:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,10 @@ class Student
         FROM students 
         WHERE name = ?
         LIMIT 1
-        SQL
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
