0391fb82e2c802bfb0ac1837396196af1e0a75cc
remote-torrent-6747 <abbyc@sky.com>
2021-03-16 21:21:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,12 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
 
   def initialize (name, grade, id = nil)
     @name = name
     @grade = grade
-    @id = id 
+    @id = id
   end
 
 
