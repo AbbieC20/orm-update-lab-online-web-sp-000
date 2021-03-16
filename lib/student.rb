7d3cdbc4d5fbc4c6d056cb653a32c006e00ebbc0
remote-torrent-6747 <abbyc@sky.com>
2021-03-16 21:41:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize (name, grade, id = nil)
+  def initialize (id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
@@ -47,4 +47,7 @@ class Student
     student
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
 end
