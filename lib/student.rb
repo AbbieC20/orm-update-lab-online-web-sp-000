e28a17e065875b4454fb0bc011cd211cb8efbd23
remote-torrent-6747 <abbyc@sky.com>
2021-03-16 21:25:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize (name, grade, id = nil)
     @name = name
@@ -13,13 +13,13 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
+        id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
         )
-        SQL 
+        SQL
       DB 
-  end 
+  end
 
 
 end
